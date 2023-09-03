73b452743f388a70a850326a4f864e200244c45f
evgen122 <evg.khristenko@gmail.com>
2023-09-03 23:28:51 +0300

    data

--- src/index.js
+++ src/index.js
@@ -8,23 +8,36 @@ import Notiflix from 'notiflix';
 
 const elements = {
   serchForm: document.querySelector('#search-form'),
+  inputData: document.querySelector('input[name=searchQuery]'),
 };
 const urlData = 'https://pixabay.com/api/';
+
 elements.serchForm.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
-  const options = {
+  evt.preventDefault();
+  //   const dataInput = evt.currentTarget.textContent;
+
+  //   console.log(elements.inputData.value);
+
+  const options = ({ key, q, imageType, orientation, safesearch } = {
     key: '39226453-68311701c5a3afc9a6c056ab8',
-    // q,
-    image_type: 'photo',
+    q: elements.inputData.value,
+    imageType: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-  };
-  options.q = evt.currentTarget.textContent;
+  });
 
-  const urlSerch = ``;
+  console.log(key, q, imageType, orientation, safesearch);
 
-  fetchS(urlSerch);
+  //   options.q = evt.currentTarget.textContent;
+
+  //   const urlSerch =
+  //     `${urlData}?, {${options}}`;
+  //   console.log(urlSerch);
+
+  //   fetchS(urlSerch);
+  //   console.log(fetchS);
 }
 
 function fetchS(urlSerch) {}
