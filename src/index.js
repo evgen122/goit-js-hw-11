cb0a461d2d58ee6b67e16a9bb9f35cc645442187
evgen122 <evg.khristenko@gmail.com>
2023-09-04 19:55:36 +0300

    ___

--- src/index.js
+++ src/index.js
@@ -7,23 +7,27 @@ import Notiflix from 'notiflix';
 
 // Ваш API-ключ:39226453-68311701c5a3afc9a6c056ab8
 
-const elements = {
+const elements = ({ serchForm, inputData, galleryImg } = {
   serchForm: document.querySelector('#search-form'),
   inputData: document.querySelector('input[name=searchQuery]'),
-};
+  galleryImg: document.querySelector('.gallery'),
+});
+
 const urlData = 'https://pixabay.com/api/';
 
-elements.serchForm.addEventListener('submit', handlerSubmit);
+// elements.serchForm.addEventListener('submit', handlerSubmit);
+serchForm.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
   evt.preventDefault();
   //   const dataInput = evt.currentTarget.value;
   //   const dataInput = evt.target.value;
   //   console.log('dataInput', dataInput);
-
+  const inputData = evt.currentTarget.elements[0].value;
   const options = ({ key, q, imageType, orientation, safesearch } = {
     key: '39226453-68311701c5a3afc9a6c056ab8',
-    q: elements.inputData.value,
+    q: 'cat',
+    // q: evt.currentTarget.elements[0].value,
     imageType: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
@@ -33,21 +37,100 @@ function handlerSubmit(evt) {
 
   //   options.q = evt.currentTarget.textContent;
 
-  const urlSerch = `${urlData}?key=${key}&q=${q}&image-type=${imageType}&orientation=${orientation}&safesearch=${safesearch}}`;
-  console.log(urlSerch);
+  const urlSerch = `${urlData}?key=${key}&q=${q}&image-type=${imageType}&orientation=${orientation}&safesearch=${safesearch}`;
+  // const urlSerch = urlData;
+  // console.log(urlSerch);
 
-  fetchS(urlSerch);
-  //   console.log(fetchS);
+  const dataArr = fetchS(urlSerch);
+  // return dataArr;
+  console.log(dataArr);
+
+  formGallery(dataArr);
+  //   choiceImg(dataArr);
 }
 
 async function fetchS(urlSerch) {
   try {
-    const response = await axios.get('urlSerch');
+    const response = await axios.get(urlData, {
+      params: {
+        key: '39226453-68311701c5a3afc9a6c056ab8',
+        q: 'cat',
+        // q: evt.currentTarget.elements[0].value,
+        imageType: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
+    });
+    console.log(response.data.hits);
+    // const datai = response.data.hits;
     console.log(response);
+    return response.data.hits;
+    // const respData = await response.data.hits;
   } catch (error) {
-    console.error(error);
+    console.error(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return error;
   }
 }
+
+function formGallery(dataArr) {}
+// вставить разметку для массива объектов
+// разметку обернуть в мар, вставить элементы массива
+// const marcupGallery = dataArr
+//   .map(
+//     ({
+//       hits: {
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       },
+//     }) => {
+// const dataInfo = ({
+// webformatURL,
+// largeImageURL,
+// tags,
+// likes,
+// views,
+// comments,
+// downloads,
+// } = {
+//   webformatURL: dataArr.hits.webformatURL,
+//   largeImageURL: dataArr.hits.largeImageURL,
+//   tags: dataArr.hits.tags,
+//   likes: dataArr.hits.likes,
+//   views: ddataArrta.hits.views,
+//   comments: dataArr.hits.comments,
+//   downloads: dataArr.hits.downloads,
+// });
+//         const photoCard = `<div class="photo-card">
+//     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes</b>${likes}
+//       </p>
+//       <p class="info-item">
+//         <b>Views</b>${views}
+//       </p>
+//       <p class="info-item">
+//         <b>Comments</b>${comments}
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads</b>${downloads}
+//       </p>
+//     </div>
+//   </div>`;
+//       }
+//     )
+//     .json();
+//   galleryImg.insertAdjacentHTML('beforeend', marcupGallery);
+// }
+
+// function choiceImg(dataArr) {}
 
 //   async function getUser() {
 //     try {
